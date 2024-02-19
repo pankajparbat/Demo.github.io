e9acea589d63ba1841a423c230ddb2bf8839b3ad
Pankaj Dashrath Parbat <121680162+pankajparbat@users.noreply.github.com>
2024-02-19 21:50:06 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -146,8 +146,7 @@ $(document).ready(function () {
     s.parentNode.insertBefore(po, s);
 
     for (var i = 0; i < share_bar.length; i++) {
-        var html = '<iframe allowtransparency="true" frameborder="0" scrolling="no"' +
-            'src="https://platform.twitter.com/widgets/tweet_button.html?url=' + encodeURIComponent(window.location) + '&amp;text=' + encodeURIComponent(document.title) + '&amp;via=ramswarooppatra&amp;hashtags=ramandantara&amp;count=horizontal"' +
+        var html = '<iframe allowtransparency="true" frameborder="0" scrolling="no"' + encodeURIComponent(window.location) + '&amp;text=' + encodeURIComponent(document.title) + '&amp;via=ramswarooppatra&amp;hashtags=ramandantara&amp;count=horizontal"' +
             'style="width:105px; height:21px;">' +
             '</iframe>' +
 
